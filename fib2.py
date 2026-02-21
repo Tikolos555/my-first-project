146f97609964a7db2d0dcccb2b6c65a42756109a
Tikolos555 <mishagamaun888@gmail.com>
2026-02-21 10:19:22 +0700

    2 вида фибоначи

---
+++ fib2.py
@@ -0,0 +1,22 @@
+
+def fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    a, b = 0, 1
+    for _ in range(2, n + 1):
+        a, b = b, a + b
+    return b
+    
+    
+
+def fibrec(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibrec(n-1) + fibrec(n-2)
+    
+    
